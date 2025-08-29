b87cd990ff4f849322decf33e8501aac9a428a29
Accurexx <dominik.stauber@chello.at>
2025-08-29 18:43:20 +0200

    #EEG028
    *) New patchoperations

---
+++ MedievalOverhaul-Royalty/MORSettings.cs
@@ -0,0 +1,39 @@
+﻿using Verse;
+using UnityEngine;
+
+namespace MedievalOverhaulRoyalty
+{
+    public class MORSettings : ModSettings
+    {
+        public bool patchEnabled = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref patchEnabled, "patchEnabled", true);
+            base.ExposeData();
+        }
+    }
+
+    public class MORMod : Mod
+    {
+        public static MORSettings settings;
+
+        public MORMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<MORSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Patch aktivieren (PatchSequenceA)", ref settings.patchEnabled);
+            listingStandard.End();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Medieval Overhaul Royalty";
+        }
+    }
+}
